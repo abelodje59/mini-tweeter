ce75fcab7bd1b90a9ad7b6e7d1633db0c20d9e86
Abel <abelodje59gmail.com>
2023-09-20 13:36:58 +0200

    updates

---
+++ tests/conftest.py
@@ -0,0 +1,32 @@
+import os
+from typing import Iterator
+import asyncio
+
+import pytest
+from fastapi.testclient import TestClient
+from tortoise.contrib.test import finalizer, initializer
+
+
+from core.config import settings
+from main import app
+from typing import Generator
+
+DB_URL = "sqlite://:memory:"
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    return asyncio.get_event_loop()
+
+
+@pytest.fixture(scope="session")
+def client() -> Generator:
+    initializer(
+        db_url=DB_URL,
+        modules=["models"],
+    )
+
+    with TestClient(app) as test_client:
+        yield test_client
+
+    finalizer()
